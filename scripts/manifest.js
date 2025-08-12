2a522a70c12e01abdc0ae1ebdac18af780809301
秋叶ゞ <1302580865@qq.com>
2025-08-12 11:26:32 +0800

    fix：manifest.js

--- scripts/manifest.js
+++ scripts/manifest.js
@@ -3,7 +3,7 @@ import { getManifest } from '../src/manifest'
 import { log, r } from './utils'
 
 export async function writeManifest() {
-  await fs.writeJSON(r('build/manifest.json'), await getManifest(), { spaces: 2 })
+  await fs.writeJSON(r('extensions/manifest.json'), await getManifest(), { spaces: 2 })
   log('PRE', 'write manifest.json')
 }
 
